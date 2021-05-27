ee4d236bafb4c21b2d5422f38f9ddfb95fc2afa6
Cheedge <cheedgelee@gmail.com>
2021-05-27 15:29:13 +0200

    GMM scratch

--- GMM/GaussianMixModle.py
+++ GMM/GaussianMixModle.py
@@ -25,8 +25,8 @@ class GMM:
         self.X = X
         self.K = K
         N, D = X.shape
-        self.mu = np.random.rand(K, D)
-        self.cov = np.random.rand(K, D, D)
+        self.mu = np.random.randn(K, D)
+        self.cov = np.random.randn(K, D, D)
         self.phi = np.random.randn(K, N)
         self.z_hat = np.empty(N)
 
@@ -41,54 +41,84 @@ class GMM:
         xi|zi==j ~ N(mu_j, cov_j)
         '''
         N, D = self.X.shape
-        self.w = np.empty(N, self.K)
-        Pzjbx = np.empty(N, self.K)
-        sum_Pxbzj = 0
-        for j in range(1, self.K+1):
+        self.w = np.empty([N, self.K])
+        Pzjbx = np.empty([N, self.K])
+        for j in range(self.K):
+            sum_j_Pxbzj = 0
             # [ N ]
-            Pzj = self.phi
+            Pzj = self.phi[j]
             # sum([ N, D ]@[ D, D ]*[ N, D ], axis=1)=sum([ N, D ],1)=[ N ]
-            #Pxbzj = 1/((2*np.pi)**(D/2)*np.sqrt(self.cov[ j ])) * np.exp(-0.5*np.sum((self.X-self.mu[ j ])@np.invert(self.cov[ j ])*(self.X - self.mu[ j ]), axis=1)).ravel()
-            Pxbzj = multivariate_normal.pdf(self.X, mean=self.mu[ j ], cov=self.cov[ j ])
+            # notice: np.invert: element invert.
+            coeff = 1/((2*np.pi)**(D/2)*np.sqrt(np.abs(np.linalg.det(self.cov[ j ]))))
+            exppart = np.exp(-0.5*np.sum((self.X-self.mu[ j ])@np.linalg.inv(self.cov[ j ])*(self.X - self.mu[ j ]), axis=1))
+            Pxbzj =  coeff * exppart.ravel()
+            # must be positive semidefined(PSD) matrix.
+            #Pxbzj = multivariate_normal.pdf(self.X, mean=self.mu[ j ], cov=self.cov[ j ])
             # [ N ]
             Pzjbx[ :, j ] = Pxbzj * Pzj
             # [ N ]
-            sum_Pxbzj += Pzjbx[ :, j ]
-            self.w[ :, j ] = Pzjbx[ :, j ]/sum_Pxbzj
-        return self.w, sum_Pxbzj
+            sum_j_Pxbzj += Pzjbx[ :, j ]
+            self.w[ :, j ] = Pzjbx[ :, j ]/sum_j_Pxbzj
+        #self.w = Pzjbx/sum_Pxbzj.ravel()
+        return self.w
 
     def M_step(self):
         '''
         update mu, cov, phi
         phi_j = (1/N) *sum_i wj^i
         mu_j = sum_i wj^i*xi /sum_i wj^i
-        cov_j = sum_i (xi-muj)T(xi-muj)* wj^i / sum_i wj^i
+        cov_j = sum_i (xi-muj)(xi-muj)T* wj^i / sum_i wj^i
         '''
         N, D = self.X.shape
         sum_w = np.sum(self.w, axis=0)
-        for j in range(1, self.K+1):
+        for j in range(self.K):
             self.phi[ j ] = (1/N) * sum_w[ j ]
             # wj:[ N ]; X:[ N, D ]; mu:[ D ]
-            self.mu[ j ] = np.sum(self.X*self.w[ j ], axis=0)/sum_w[ j ]
-            self.cov[ j ] = np.sum(np.transpose(self.X - self.mu)@(self.X -self.mu)*self.w[ j ], axis=0)/sum_w[ j ]
+            # notice: should be [N, D]*[N, 1] not [N, D]*[N,]
+            self.mu[ j ] = np.sum(self.X*self.w[ :, j ].reshape(N, 1), axis=0)/sum_w[ j ]
+            # [N, D]T[N, D]
+            self.cov[ j ] = np.sum((self.X - self.mu[j]).transpose()@((self.X -self.mu[j])*self.w[ :, j ].reshape(N, 1)), axis=0)/sum_w[ j ]
 
-    def LogLikelihood(self):
+    def LogLikelihood(self, z):
         '''
-        joint likelihood P(x, z)=P(x|z)P(z)
+        Joint likelihood: sum_i P(xi, zi)=P(xi|zi)P(zi)
+        zi=j->[0,K)
+        P(zi) = phi(zi)
+        P(xi|zi) = N(mu_zi, cov_zi)
         '''
-        #return lh
-        pass
+        N, D = self.X.shape
+        # [N]
+        Pzi = self.phi[z]
+        # [D]
+        m = self.mu[z]
+        # [D, D]
+        s = self.cov[z]
+        print(z, m.shape, s.shape)
+        coeff_l = 1/((2*np.pi)**(D/2)*np.sqrt(np.abs(np.linalg.det(s))))
+        # sum[N, D]@[D, D]*[N, D]->[N]
+        exppart_l = np.exp(-0.5*np.sum((self.X-m)@np.linalg.inv(s)*(self.X - m), axis=1))
+        Pxbgz = coeff_l * exppart_l
+        # [N]
+        Pxz = Pxbgzb * Pzi
+        # float
+        lh = np.sum(Pxz, axis=0)
+        lglh = 0
+        for i in range(N):
+            lglh += np.log(Pxz[i])
+        return lh, lglh
 
     def forward(self):
         lt0 = 0
+        eps = 1E-6
         while(True):
-            omega, l = E_step()
-            lt = np.log(l)
-            M_step()
-            #lt = LogLikelihood()
-            if np.abs(lt - lt0) < eps:
+            omegas = self.E_step()
+            self.M_step()
+
+            # omeags[ N, K ]
+            pred_z = np.argmax(omegas, axis=1)
+            lt, loglt = self.LogLikelihood(pred_z)
+            if np.abs(loglt - lt0) < eps:
                 break
             else:
-                lt0 = lt
-        # omeag[ N, K ]
-        pred_z = np.argmax(omega, axis=1)
+                lt0 = loglt
+        return pred_z
