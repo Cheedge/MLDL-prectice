4085a53f60b1c4f1467c87d9c49a8bca05797911
Cheedge <cheedgelee@gmail.com>
2021-05-29 22:03:03 +0200

    fix sigularity

--- GMM/GaussianMixModle.py
+++ GMM/GaussianMixModle.py
@@ -25,11 +25,26 @@ class GMM:
         self.X = X
         self.K = K
         N, D = X.shape
-        self.mu = np.random.randn(K, D)
-        self.cov = np.random.randn(K, D, D)
-        self.phi = np.random.randn(K, N)
-        self.z_hat = np.empty(N)
+        #self.mu = np.random.rand(K, D)
+        # [ K, D ]choose one point of every row as mean
+        self.mu = np.random.choice(X.flatten(), (K, D))
+        self.cov = self.makePSD(K, D)
+        # [ K, N ]
+        self.phi = np.repeat((np.ones(K)/K).reshape(K, 1), N, axis=1)
+        self.z_hat = np.zeros(N)
+        self.eps = 1E-6
 
+    @staticmethod
+    def makePSD(K, D):
+        a = np.random.randn(D, D)
+        s_matrix = a@a.T
+        s_matrix = s_matrix[np.newaxis, :]
+        for i in range(K-1):
+            A = np.random.rand(D, D)
+            # A.T@A or A@A.T is PSD
+            s = A@A.T
+            s_matrix = np.concatenate((s_matrix, s[np.newaxis, :]), axis=0)
+        return s_matrix
 
     def E_step(self):
         '''
@@ -42,24 +57,31 @@ class GMM:
 
         '''
         N, D = self.X.shape
-        self.w = np.empty([N, self.K])
-        Pzjbx = np.empty([N, self.K])
+        self.w = np.zeros([N, self.K])
+        Pzjbx = np.zeros([N, self.K])
+        reg_cov = self.eps * np.identity(D)
         for j in range(self.K):
             sum_j_Pxbzj = 0
             # [ N ]
             Pzj = self.phi[j]
             # sum([ N, D ]@[ D, D ]*[ N, D ], axis=1)=sum([ N, D ],1)=[ N ]
             # notice: np.invert: element invert.
-            coeff = 1/((2*np.pi)**(D/2)*np.sqrt(np.abs(np.linalg.det(self.cov[ j ]))))
-            exppart = np.exp(-0.5*np.sum((self.X-self.mu[ j ])@np.linalg.inv(self.cov[ j ])*(self.X - self.mu[ j ]), axis=1))
-            Pxbzj =  coeff * exppart.ravel()
-            # must be positive semidefined(PSD) matrix.
-            #Pxbzj = multivariate_normal.pdf(self.X, mean=self.mu[ j ], cov=self.cov[ j ])
+            #coeff = 1/((2*np.pi)**(D/2)*np.sqrt(np.abs(np.linalg.det(self.cov[ j ]))))
+            #exppart = np.exp(-0.5*np.sum((self.X-self.mu[ j ])@np.linalg.inv(self.cov[ j ]+reg_cov)*(self.X - self.mu[ j ]), axis=1))
+            #Pxbzj =  coeff * exppart.ravel()
+            # must be positive semidefined(PSD) matrix: means invertable
+            # [ N ]
+            Pxbzj = multivariate_normal.pdf(self.X, mean=self.mu[ j ], cov=self.cov[ j ]+reg_cov)
             # [ N ]
             Pzjbx[ :, j ] = Pxbzj * Pzj
             # [ N ]
-            sum_j_Pxbzj += Pzjbx[ :, j ]
-            self.w[ :, j ] = Pzjbx[ :, j ]/sum_j_Pxbzj
+            #sum_j_Pxbzj += Pzjbx[ :, j ]
+            #self.w[ :, j ] = Pzjbx[ :, j ]/(sum_j_Pxbzj+self.eps)
+        # sum over i and j: float
+        sum_Pxbzj = np.sum(Pzjbx)
+        # [N, K]/float = [N, K]
+        self.w = Pzjbx/sum_Pxbzj
+        #print(sum_Pxbzj, Pzjbx.shape)
         #self.w = Pzjbx/sum_Pxbzj.ravel()
         return self.w
 
@@ -79,22 +101,21 @@ class GMM:
         Singularity!
         '''
         N, D = self.X.shape
+        # sum[ N, K ]->[ K ]
         sum_w = np.sum(self.w, axis=0)
         for j in range(self.K):
-            covj = np.zeros([D, D])
             self.phi[ j ] = (1/N) * sum_w[ j ]
             # wj:[ N ]; X:[ N, D ]; mu:[ D ]
             # notice: should be [N, D]*[N, 1] not [N, D]*[N,]
-            self.mu[ j ] = np.sum(self.X*self.w[ :, j ].reshape(N, 1), axis=0)/sum_w[ j ]
+            self.mu[ j ] = np.sum(self.X*self.w[ :, j ].reshape(N, 1), axis=0)/(sum_w[ j ]+self.eps)
+
             # [N, D]T[N, D]
-            #self.cov[ j ] = np.sum((self.X - self.mu[j]).transpose()@((self.X -self.mu[j])*self.w[ :, j ].reshape(N, 1)), axis=0)/sum_w[ j ]
+            #self.cov[ j ] = (self.X - self.mu[j]).transpose()@((self.X -self.mu[j])*self.w[ :, j ].reshape(N, 1))/(sum_w[ j ]+self.eps)
+            covj = np.zeros([D, D])
             for i in range(N):
-                print(covj,'\t')
-                print(self.X[i],'\t',self.mu[j],'\n')
                 # X_i:[D], mu_j:[D], w^i_j:[N, K]->1
-                covj += (self.X[i] - self.mu[j]).reshape(D, 1)@((self.X[i] -self.mu[j]).reshape(1, D))*self.w[i, j]
+                covj += (self.X[i] - self.mu[j]).reshape(D, 1)@((self.X[i] -self.mu[j]).reshape(1, D))*self.w[i, j]/(sum_w[ j ]+self.eps)
             self.cov[j] = covj
-            print(self.cov[j])
 
     def LogLikelihood(self, z):
         '''
@@ -105,27 +126,41 @@ class GMM:
 
         '''
         N, D = self.X.shape
-        # [N]
-        Pzi = self.phi[z]
-        # [D]
+        # [K, D]->[N, D]
         m = self.mu[z]
-        # [D, D]
+        # [K, D, D]->[N, D, D]
         s = self.cov[z]
+        '''
         coeff_l = 1/((2*np.pi)**(D/2)*np.sqrt(np.abs(np.linalg.det(s))))
-        # sum[N, D]@[D, D]*[N, D]->[N]
-        exppart_l = np.exp(-0.5*np.sum((self.X-m)@np.linalg.inv(s)*(self.X - m), axis=1))
+        # sum[N, D]@[N, D, D]*[N, D]->[N]
+        exppart_l = np.exp(-0.5*np.sum((self.X-m)@np.linalg.inv(s)*(self.X - m), axis=2))
+        print(exppart_l.shape, '\n',coeff_l.shape, '\n')
         Pxbgz = coeff_l * exppart_l
         # [N]
-        Pxz = Pxbgzb * Pzi
+        print(Pxbgz.shape, '\n',Pzi.shape)
+        Pxz = Pxbgz * Pzi
         # float
         lh = np.sum(Pxz, axis=0)
-        lglh = 0
+        '''
+        lh = 0
         for i in range(N):
-            lglh += np.log(Pxz[i])
-        return lh, lglh
+            # [K, N]->[N]
+            Pzi = self.phi[z[i], i]
+            #coeff_l = 1/((2*np.pi)**(D/2)*np.sqrt(np.abs(np.linalg.det(s[ i ]))))
+            ## sum[D]@[D, D]*[D]->float
+            #exppart_l = np.exp(-0.5*np.sum((self.X[ i ]-m[ i ])@np.linalg.inv(s[ i ]+self.eps*np.identity(D))*(self.X[ i ] - m[ i ]), axis=0))
+            #Pxbgz = coeff_l * exppart_l
+            #Pxz = Pxbgz * Pzi[i]
+
+            Pxbgz = multivariate_normal.pdf(self.X[ i ], m[ i ], s[ i ]+self.eps*np.identity(D))
+            Pxz = Pxbgz * Pzi
+
+            lh += Pxz
+        loglh = np.log(lh)
+        return loglh
 
     def forward(self):
-        lt0 = 0
+        lglt0 = 0
         eps = 1E-6
         while(True):
             omegas = self.E_step()
@@ -133,9 +168,10 @@ class GMM:
 
             # omeags[ N, K ]
             pred_z = np.argmax(omegas, axis=1)
-            lt, loglt = self.LogLikelihood(pred_z)
-            if np.abs(loglt - lt0) < eps:
+            loglt = self.LogLikelihood(pred_z)
+            print('in code', pred_z)
+            if np.abs(loglt - lglt0) < eps:
                 break
             else:
-                lt0 = loglt
+                lglt0 = loglt
         return pred_z
